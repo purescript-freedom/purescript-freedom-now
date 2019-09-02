441961c5696365fbaede2d8378d4db65dee591d5
Shinya Takahashi <s.takahashi313@gmail.com>
2019-09-02 14:20:45 +0900

    Change interface

--- example/Main.purs
+++ example/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 
-import Data.JSDate (JSDate, now, toString)
+import Data.DateTime (DateTime)
+import Data.JSDate (fromDateTime, toString)
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
+import Effect.Now (nowDateTime)
 import Freedom as Freedom
 import Freedom.Markup as H
 import Freedom.Now (currentTime)
@@ -12,7 +14,7 @@ import Freedom.Subscription (Subscription)
 import Freedom.TransformF.Simple (VQueryF, transformF, reduce)
 import Freedom.VNode (VNode)
 
-type State = JSDate
+type State = DateTime
 
 type Sub = Subscription VQueryF State
 
@@ -20,22 +22,22 @@ type Html = VNode VQueryF State
 
 main :: Effect Unit
 main = do
-  jsdate <- now
+  dateTime <- nowDateTime
   Freedom.run
     { selector: "#app"
-    , initialState: jsdate
+    , initialState: dateTime
     , subscriptions: [ currentTime' ]
     , transformF
     , view
     }
 
 currentTime' :: Sub
-currentTime' = currentTime (Milliseconds 3000.0) \jsdate -> do
-  reduce $ const jsdate
+currentTime' = currentTime (Milliseconds 3000.0) \dateTime -> do
+  reduce $ const dateTime
 
 view :: State -> Html
-view jsdate =
+view dateTime =
   H.el $ H.div # H.kids
     [ H.el $ H.h1 # H.kids [ H.t "Now Demo" ]
-    , H.el $ H.p # H.kids [ H.t $ toString jsdate ]
+    , H.el $ H.p # H.kids [ H.t $ toString $ fromDateTime dateTime ]
     ]
--- src/Freedom/Now.purs
+++ src/Freedom/Now.purs
@@ -3,10 +3,11 @@ module Freedom.Now where
 import Prelude
 
 import Control.Monad.Free.Trans (FreeT)
+import Data.DateTime (DateTime)
 import Data.Int (ceil)
-import Data.JSDate (JSDate, now)
 import Data.Time.Duration (Milliseconds(..))
 import Effect.Aff (Aff, launchAff_)
+import Effect.Now (nowDateTime)
 import Effect.Timer (setInterval)
 import Freedom.Subscription (Subscription, subscription)
 
@@ -14,10 +15,10 @@ currentTime
   :: forall f state
    . Functor (f state)
   => Milliseconds
-  -> (JSDate -> FreeT (f state) Aff Unit)
+  -> (DateTime -> FreeT (f state) Aff Unit)
   -> Subscription f state
 currentTime (Milliseconds n) onUpdate =
   subscription \transform -> do
     void
       $ setInterval (ceil n)
-      $ now >>= onUpdate >>> transform >>> launchAff_
+      $ nowDateTime >>= onUpdate >>> transform >>> launchAff_
